38bb54a7c5e73fff3fae6f94bae2aa6c9c10815a
Ashish <ashishgupta57347@gmail.com>
2024-10-23 22:16:08 +0530

    complete my dom project

---
+++ index.js
@@ -0,0 +1,14 @@
+//console.log("Ashish");
+
+
+let div = document.querySelector(".div1");
+div.style.height = "200px";
+div.style.backgroundColor = "Yellow";
+div.style.width = "300px"
+div.style.margin = "20px";
+div.innerText = "Hello, I'm a div!";
+div.style.padding = "10px"
+div.style.color = "blue"
+div.style.fontWeight = "bold"
+div.style.fontSize = "18px"
+
